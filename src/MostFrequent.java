2bc6cf4eed0fc311d947c2ca0e50a38c6169e726
Dave <Dave.Troupe@gmail.com>
2017-05-22 08:50:11 -0400

    Initial commit

---
+++ src/MostFrequent.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Dave on 5/22/17.
+ */
+public class MostFrequent {
+    /*
+     *  Complete this class, it is your choice if you want to use an
+     *  object or just static methods. Here is a brief description of
+     *  what the most frequent classifier should do:
+     *
+     *  1. Find the most frequent label in the training data (0, or 1)
+     *  2. Classify every data point in the testing data with the most frequent label
+     *  3. Calculate the accuracy of your predictions
+     *
+     *          Why would anyone use a classifier that's so simple?
+     */
+
+}
